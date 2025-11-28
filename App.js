6dbc39fef6b797f4045cecdfdb9dc3b4bef58a42
Nibras Ahmed <odaabultum116@gmail.com>
2025-11-29 02:47:04 +0300

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function App(){
-  return (
-    <div style={{fontFamily:'Arial, Helvetica, sans-serif', padding:40}}>
-      <h1>ELSIP Prototype (PWA Demo)</h1>
-      <p>Visit /jobs and /ai endpoints to demo functionality.</p>
-    </div>
-  );
-}
