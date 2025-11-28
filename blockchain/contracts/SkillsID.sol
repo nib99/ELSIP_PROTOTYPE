945ef975ccacfcedfba441d59dfc92a59b0be44d
Nibras Ahmed <odaabultum116@gmail.com>
2025-11-29 02:41:48 +0300

    Add files via upload

--- blockchain/contracts/SkillsID.sol
+++ blockchain/contracts/SkillsID.sol
@@ -1,1 +1,14 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract SkillsID is ERC721 {
+  uint256 public nextId;
+  mapping(uint256=>string) public metadata;
+  constructor() ERC721('ELSIP SkillsID','ESID'){}
+  function mint(address to, string memory data) external returns(uint256){
+    uint256 id = ++nextId;
+    _mint(to, id);
+    metadata[id] = data;
+    return id;
+  }
+}
