03db688a9a82be4cb6554d2076e16d46e224423d
Nibras Ahmed <odaabultum116@gmail.com>
2025-11-29 02:46:22 +0300

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.get('/health', (req,res)=>res.json({status:'ok'}));
-app.listen(8080, ()=>console.log('Frontend served on 8080'));
